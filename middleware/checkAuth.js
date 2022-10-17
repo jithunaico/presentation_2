1f8281ab318403fc3508a31b058d54a15d7e4aa0
jithu vs <115207737+jithunaico@users.noreply.github.com>
2022-10-17 12:31:06 +0530

    Update checkAuth.js

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,24 +1,30 @@
-module.exports  = async (req,res,next)=>{
-   const token  = re.header( 'x-auth-token');
-   if(!token) {
-        return res.status(422).json({
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+    const token = req.header('x-auth-token')
+
+    // CHECK IF WE EVEN HAVE A TOKEN
+    if(!token){
+        res.status(401).json({
             errors: [
                 {
-                    msg: "no token",
-                }
-            ]
-        })
-    }
-    try{
-        let user  =jwt.verify(token,'qweqweqeeqeqe123123123213')
-    }catch(error){
-        return res.status(422).json({
-            errors: [
-                {
-                    msg: "invalid token ",
+                    msg: "No token found"
                 }
             ]
         })
     }
 
+    try {
+        const user = await jwt.verify(token,  "qweqweqeeqeqe123123123213")
+        req.user = user.email
+        next()
+    } catch (error) {
+        res.status(400).json({
+            errors: [
+                {
+                    msg: 'Invalid Token'
+                }
+            ]
+        })
+    }
 }
